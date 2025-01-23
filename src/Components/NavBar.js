6d4a57dd3845ba8a938e42dea4c0e2c5600ff1d1
livio.macaj <livio.macaj@cit.edu.al>
2025-01-23 10:36:33 +0000

    fix

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,43 +3,48 @@ import { useState } from "react";
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
-  const navbar = document.getElementById('navbar')
-  
-  
-addEventListener('mousedown',(e)=>{
-  /* eslint-disable-line no-restricted-globals */
-  if  (isOpen){
-    if(e.target.id != 'navbar'){
-      toggle()
+  const root = document.getElementById("root");
+
+  root.addEventListener("mousedown", (e) => {
+    if (isOpen) {
+      if (e.target.id != "navbar") {
+        toggle();
+      }
     }
-  }
-})
+  });
 
   return (
-      <div id="navbar"
+    <div
+      id="navbar"
+      className={
+        isOpen
+          ? " bg-white shadow-lg text-right pt-10 pr-10  rounded-tr-3xl  sticky duration-300 top-3 h-dvh"
+          : "p-3 duration-300"
+      }
+    >
+      <button
         className={
           isOpen
-            ? " bg-white shadow-lg text-right pt-10 pr-10  rounded-tr-3xl  sticky duration-300 top-3 h-dvh"
-            : "p-3 duration-300"
+            ? "absolute top-3 p-1 right-3 rounded-full bg-gray-100 hover:invert hover:scale-110 active:bg-gray-100 material-symbols-outlined duration-300"
+            : "sticky top-3 rounded-full overflow-hidden text-5xl bg-white hover:invert active:bg-gray-100 material-symbols-outlined rotate-180 duration-300"
+        }
+        onClick={toggle}
+      >
+        arrow_left
+      </button>
+      <nav
+        id="navbar"
+        onClick={toggle}
+        className={
+          isOpen ? "sticky top-3 flex flex-col gap-5 mt-4" : " w-0 hidden "
         }
       >
-        <button
-          className={
-            isOpen
-              ? "absolute top-3 p-1 right-3 rounded-full bg-gray-100 hover:invert hover:scale-110 active:bg-gray-100 material-symbols-outlined duration-300"
-              : "sticky top-3 rounded-full overflow-hidden text-5xl bg-white hover:invert active:bg-gray-100 material-symbols-outlined rotate-180 duration-300"
-          }
-          onClick={toggle}
-        >
-          arrow_left
-        </button>
-        <nav id="navbar" onClick={toggle} className={isOpen ? "sticky top-3 flex flex-col gap-5 mt-4" : " w-0 hidden "}>
-          <CustomLink to="/">Home</CustomLink>
-          <CustomLink to="/about">About</CustomLink>
-          <CustomLink to="/contact">Contact</CustomLink>
-          <CustomLink to="/works">Works</CustomLink>
-        </nav>
-      </div>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+        <CustomLink to="/contact">Contact</CustomLink>
+        <CustomLink to="/works">Works</CustomLink>
+      </nav>
+    </div>
   );
 }
 
