3f459c81a614ba94ac22676c54e1e55e31dfc369
livio.macaj <livio.macaj@cit.edu.al>
2025-02-11 15:08:49 +0000

    Changes

--- src/App.js
+++ src/App.js
@@ -5,47 +5,56 @@ import ContactPage from "./Pages/ContactPage";
 import PortofolioPage from "./Pages/PortofolioPage";
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
-import { useState, useEffect } from "react";
-import { AnimatePresence, motion } from 'framer-motion';
-import { Routes, Route, useLocation, useNavigationType } from 'react-router-dom';
+import { useState, useEffect, useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import {
+  Routes,
+  Route,
+  useLocation,
+  useNavigationType,
+} from "react-router-dom";
 
 const AnimatedPage = ({ children, direction }) => {
-  const initialX = direction === 'forward' ? '100%' : direction === 'backward' ? '-100%': 0;
-  const exitX = direction === 'forward' ? '-100%' : direction === 'backward' ? '100%': 0;
+  const initialX =
+    direction === "forward" ? "100%" : direction === "backward" ? "-100%" : 0;
+  const exitX =
+    direction === "forward" ? "-100%" : direction === "backward" ? "100%" : 0;
   return (
     <motion.div
       initial={{ x: initialX }}
       animate={{ x: 0 }}
       exit={{ x: exitX }}
       transition={{ duration: 0.5 }}
-      style={{ width: '100%', height: '100%' }}
+      style={{ width: "100%", height: "100%" }}
     >
       {children}
     </motion.div>
   );
-}; 
+};
 
 function App() {
   const location = useLocation();
   const navigationType = useNavigationType();
-  const [direction, setDirection] = useState('forward');
+  const [direction, setDirection] = useState("forward");
+  const constraintsRef = useRef(null);
+
 
   useEffect(() => {
-    setDirection(navigationType === 'PUSH' ? 'forward' : 'backward');
+    setDirection(navigationType === "PUSH" ? "forward" : "backward");
   }, [navigationType]);
 
   return (
     <>
-      <div className="flex bg-black w-full h-dvh">
+      <div className="flex bg-black w-dvw h-dvh" >
         <NavBar />
-        <div className="p-8" id="canvas">
+        <div className="p-8" id="canvas" ref={constraintsRef}>
           <AnimatePresence initial={false} mode="wait">
             <Routes location={location} key={location.pathname}>
               <Route
                 index
                 element={
                   <AnimatedPage direction={direction}>
-                    <HomePage />
+                    <HomePage constraintsRef={constraintsRef} />
                   </AnimatedPage>
                 }
               />
